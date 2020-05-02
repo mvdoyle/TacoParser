766fcf5c89251aef01f527873d33f56fb01f5a8c
mvdoyle <mdoyle@truecoders.io>
2020-05-02 10:05:06 -0500

    almost

---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LoggingKata
+{
+    public class TacoBell : ITrackable
+    {
+        public TacoBell()
+        {
+        }
+
+        public TacoBell(string name, Point location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public string Name { get; set; }
+        public Point Location { get; set; }
+    }
+}
